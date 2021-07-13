7fe6d13b7b46422c173c8da6251ff1fbad5718c5
Iresharma <iersh.sharma8@gmail.com>
2021-07-13 13:51:09 +0530

    cache moved to redis

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from flask_caching import Cache
 
 config = {
     "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
+    "CACHE_TYPE": "RedisCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_REDIS_HOST": "127.0.0.1", # Redis host running on local
 }
 
 app = Flask(__name__)
