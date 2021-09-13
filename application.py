47a58f24251c1cbf9c2ff0f9154f50ef3d4ec9d1
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2021-09-13 18:01:58 +0530

    fill db using a file upload (#18)
    * fill db using a file upload
    
    * Update __init__.py
    
    Co-authored-by: Iresharma <iersh.sharma8@gmail.com>

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
 from app import app as application
 from os import environ
+from flask import request
+
+@application.before_request
+def before_request():
+    print(request.headers)
 if __name__ == '__main__':
-    application.run(debug=environ.get("DEBUG", False), port=environ.get("PORT", 5000))
+    application.run(debug=environ.get("DEBUG", True), port=environ.get("PORT", 5000))
